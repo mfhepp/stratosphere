52cd8ff230bba84596f7ad7388fbcad75f1e4c52
Martin Hepp <mfhepp@gmail.com>
2016-10-28 09:54:49 +0200

    reverted .sh scripts

--- software/shell/detect_and_mount_usb.sh
+++ software/shell/detect_and_mount_usb.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
-# Turn off secondary power supply from S.USV
 
-# Ensure script is run as root
+# ensure script is run as root
 if [ $(id -u) -ne 0 ]
 then
     echo "Please run as root"
     exit
 fi
 
-echo "Turning off secondary power supply from S.USV"
-sudo i2cset -y 1 0x0f 0x31 
+
+sda=$(blkid | awk '{print substr($1, 1, length($1)-1)}' | grep /dev/sd)
+
+for volume in $sda
+do
+    size=$(sudo fdisk -l $volume | grep $volume | awk -F', ' '{print substr($2, 1, length($2)-6)}')
+    echo $size
+    # assume the usb partition is bigger than 30GB
+    if [ "$size" -gt "30000000000" ]
+    then
+       echo $volume" is bigger"
+       mount -t vfat -o uid=pi -o gid=pi $volume /media/usbstick
+       if mount | grep $volume > /dev/null
+       then
+	   echo $volume" has been mounted"
+       fi
+    fi       
+done
